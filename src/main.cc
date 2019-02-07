36b5e0904d550652f24f617a96b6c778620051c4
jeremy woo <kepungmoin@hotmail.com>
2019-02-07 22:03:20 +1300

    adding conanbuildinfo.cmake for configuration purposes.

--- src/main.cc
+++ src/main.cc
@@ -1,8 +1,9 @@
 #include <iostream>
+#include <boost/scoped_ptr.hpp>
 
 int main() {
 
-    std::cout << "test" << std::endl;
-    
+    std::cout << "test" << std::endl; 
+    boost::scoped_ptr<int> p {new int{100}};
+    std::cout << *p.get() << '\n';
 }
-
